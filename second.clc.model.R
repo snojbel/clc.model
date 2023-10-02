689dcd223a8c21d96c59a3d9a4a21ec40ccf816f
Izabel Eriksson Reuterborg <izer4773@user.uu.se>
2023-10-02 14:23:50 +0200

    Fixed some misses and added plotting. Think I discovered I cant add negative resoource properties. Creates NAs

--- second.clc.model.R
+++ second.clc.model.R
@@ -37,7 +37,7 @@ which(d == 1)
 
 # Full function ----------------------------------------------------------------
 
-resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0.4),ncol=1, nrow=2), fmax = 5, kA = 0.3, kJ = 0.2, mutProb=0.001, mutVar=0.1, time.steps=200, iniPA=4, iniPJ=4, nmorphs = 1){
+resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.2,0.2),ncol=1, nrow=2), fmax = 10, kA = 2, kJ = 0.2, mutProb=0.001, mutVar=0.1, time.steps=200, iniPA=4, iniPJ=4, nmorphs = 1){
   
   pop <- matrix(data = NA, ncol = 4, nrow = nmorphs)                             # Each column in this matrix is one phenotype combination.
   
@@ -46,9 +46,11 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
   pop[,3] <- iniPJ
   
   colnames(pop) <- c("Number of indivduals", "Adult trait", "Juvenile trait", "Proxy")
-  
-  stats         <- NULL                                                          #Where we will eventually save our stats and phenotypes
-  phenotypes <- matrix(NA, ncol = 4, nrow = 0)                                         
+ 
+  stats         <- matrix(data = c(0, sum(pop[,1]), nrow(pop), mean(pop[,2]), var(pop[,2]), 
+                            mean(pop[,3]), var(pop[,3])), nrow = 1, ncol = 7)                                                             #Where we will eventually save our stats and phenotypes
+  phenotypes <- matrix(data = c(0, popSize, iniPA, iniPJ), nrow = 1, ncol = 4)
+  colnames(phenotypes) <- c("Year", "Number of indivduals", "Adult trait", "Juvenile trait")                                        
 
   
   for (t in 1:time.steps){
@@ -61,9 +63,9 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
       
       for(r in 1:ncol(resProp)){                                                  # Loops through each resource to compute 
         
-        rp <- resProp[1,r]                                                        # Resource property for adult resource [2,]
+        rp <- resProp[1,r]                                                        # Resource property for adult resource [1,]
         
-        alpha     <- exp(-(adults[, 2]-rp)^2/ (2*resGen[1,1]) )                   # Compute  alphas for all adults, if different generalities for stages want to be added modify resGen
+        alpha     <- exp(-(adults[, 2]-rp)^2/ (2*resGen[1,1])^2 )                   # Compute  alphas for all adults, if different generalities for stages want to be added modify resGen
         alphaA    <- cbind(alphaA, alpha)                                         # Store alphas for all resources for all morphs
         alphaSumA <- c(alphaSumA, sum(alpha*adults[,1]))                                    # Store the sum of all alphas for each resource
         
@@ -76,7 +78,7 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
                                                                                   # fecundity and then adds them together to create the total fec..
           R  <- resFreq[1,r]
           fec <- fec + R * (alphaA[i,r]/alphaSumA[r])                             # Total fecundity is the sum of fecundities gained 
-          # from all the different resources. Which is derived
+                                                                                  # from all the different resources. Which is derived
         }                                                                         # both the ability to consume a resource, alphaA, and the 
         adults[i,4] <- fmax * (fec/(kA+fec))                                       # the total ability of all individuals to consume 
                                                                                   # said resource, alphaSumA. R = resource available
@@ -101,6 +103,7 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
       
       if(N.mut > 0){
         random.choice <- c()
+        mutation.pos <- c()
         
         for (m in 1:length(N.mut)){
         random.choice <- rmultinom(n = 1 , size = 1, prob = probs)    # Randomly chooses which morphs to mutate based on probs
@@ -143,7 +146,7 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
         
         rp <- resProp[2,r] 
         
-        alpha     <- exp(-(juveniles[,3]-rp)^2/ (2*resGen[1,1]) )
+        alpha     <- exp(-(juveniles[,3]-rp)^2/ (2*resGen[2,1])^2 )
         alphaJ    <- cbind(alphaJ, alpha)
         alphaSumJ <- c(alphaSumJ, sum(alpha) )
         
@@ -159,15 +162,15 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
           
         }                                                                         
       
-      det.sur <- sur/(kJ+sur)                                                    # this is to create a saturating function, survival can never be over 1 higher kJ means flatter curve
-      juveniles[i,1] <- as.numeric(rbinom(n = 1 , size = juveniles[i,1], prob = det.sur))   # sees how many survive given their det. survival.
+      det.sur <- sur/(kJ+sur)                                                   # this is to create a saturating function, survival can never be over 1 higher kJ means flatter curve
+      juveniles[i,1] <- rbinom(n = 1 , size = juveniles[i,1], prob = det.sur)   # sees how many survive given their det. survival.
       }
       
-    pop <- juveniles[juveniles[, 1] != 0, ]                                     # all adults die after reproducing, so the new generation is only juveniles, and all rows with zero individuals are removed.
+    pop <- juveniles[juveniles[, 1] != 0, , drop = FALSE]                        # all adults die after reproducing, so the new generation is only juveniles, and all rows with zero individuals are removed.
     
     # extract stats and phenotype ---------------------------------------------
     
-    if(!is.data.frame(pop)){                                                       # Checks whether population has reached zero, then it breaks the for loop.                                   
+    if(nrow(pop) == 0){                                                          # Checks whether population has reached zero, then it breaks the for loop.                                   
       print("Population extinction")
       break
     }
@@ -175,15 +178,15 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
     stats <- rbind(stats, c(t, sum(pop[,1]), nrow(pop), mean(pop[,2]), var(pop[,2]), 
                             mean(pop[,3]), var(pop[,3]))) 
     
-    phenotype.stats <- cbind(rep(t, nrow(pop)), pop[,1], pop[,2], pop[,3])
-    phenotypes <- rbind(phenotypes, phenotype.stats)
+    pStats <- cbind(rep(t, nrow(pop)), pop[,1], pop[,2], pop[,3])
+    phenotypes <- rbind(phenotypes, pStats)
     
       
   }
   
   #return output  ------------------------------------------------------------
   colnames(stats) <- c("year", "population size", "Number of morphs", "mean A trait", "var A", "mean J trait", "var J")
-  colnames(phenotypes) <- c("year", "Number of indivduals", "Phenotype A", "Phenotype J")
+  rownames(phenotypes) <- NULL
   return(list(stats=stats, phenotypes=phenotypes))                                 #returns both the stats and the phenotype
   
   
@@ -192,10 +195,10 @@ resourceCompetition <- function(popSize, resProp, resFreq, resGen=matrix(c(0.4,0
 
 
                     
-resource.frequency <- c(2,2,4,3,2,  # res. freq. of adults
-                        2,2,2,2,2)  # res. freq. pf juveniles
-resource.property<- c(2, 3, 4, 5, 6,          # res. property of juveniles
-                      2, 3, 4, 5, 6)          # res. property pf adults
+resource.frequency <- c(100,20,20,10,50,  # res. freq. of adults
+                        40,20,20,20,30)   # res. freq. pf juveniles
+resource.property<- c(1, 3, 4, 5, 10,            # res. property of adults
+                      2, 3, 4, 5, 7)             # res. property of juveniles
 
 resFreqMatrix <- matrix(resource.frequency, nrow=2, ncol=5, byrow = TRUE) 
 rownames(resFreqMatrix) <- c("Juvenile", "Adult")
@@ -206,14 +209,41 @@ rownames(resPropMatrix)<-c("Juvenile", "Adult")
 colnames(resFreqMatrix)  <- paste0("Resource ", 1:ncol(resPropMatrix))
 
 
-output <- resourceCompetition(resProp=resPropMatrix, resFreq=resFreqMatrix, popSize = 100, mutProb=0.001, mutVar=0.002, time.steps = 10)
+output <- resourceCompetition(resProp=resPropMatrix, resFreq=resFreqMatrix, popSize = 10, mutProb=0.001, mutVar=0.05, time.steps = 500)
 
 stats <- output$stats
 phenotypes <- output$phenotypes
 
 
 
-# Attempt at splitting function into smaller parts: -----------------------------------------------------------------------------------------
+# Plotting  -------------------------------------------------------------------
+
+par(mfrow=c(2,1))
+
+
+plot(x = stats[, 1], y = stats[, 2], xlab = "Year", ylab = "Total population size", type = "l")
+
+plot(x = stats[, 1], y = stats[, 3], xlab = "Year", ylab = "Number of Phenotypes", type = "l")
+
+par(mfrow=c(2,1))
+
+pColors <- rgb(0.7, 0.1, 0.5, alpha = (phenotypes[,2]/(100+phenotypes[,2])))
+plot(x = phenotypes[, 1], y = phenotypes[,3], col = pColors, xlab = NULL, ylab = "Adult trait")
+plot(x = phenotypes[, 1], y = phenotypes[,4], col = pColors, xlab = "Year", ylab = "Juvenile Trait")
+
+
+# Add a legend to distinguish between patches
+#legend("topright", legend=c("Patch 1", "Patch 2"), col=c(rgb((1-0.9)*0.7,0.59,0.8, alpha =0.6), rgb((2-0.9)*0.7,0.59,0.8, alpha =0.6)), pch=19)
+
+# Add a title to the plot
+#title("Individual Phenotypes Over Years for Patch 1 and Patch 2")
+
+# -------------------------------
+
+
+
+
+# Function split into smaller parts(for easier debugging): -----------------------------------------------------------------------------------------
 
 #----------------- Population initialization:
 popSize <- 15
@@ -483,25 +513,6 @@ plot(x = 1:nrow(populationSize), y = populationSize, type = "l", xlab = "Years",
 
 
 
-# Plot phenotypes (not modified yet)  -------------------------
-
-par(mfrow=c(1,1))
-
-#creating colors takes maybe to long to always be worth it.
-colors <- rgb((phenotypes[,2]-0.9)*0.7,0.59,0.8, alpha =0.1)    #Creates a vector of characters that have two different color identities based on patch idenity.
-
-plot(phenotype_data[,1], phenotypes_data[,3], pch=16, col=colors, xlab="Year", ylab="Phenotype")
-
-#one color, for computational purposes
-#plot(phenotype_data[,1], phenotype_data[,3], pch=16, col=rgb(0.8, 0.59, 0.8, alpha=0.3), xlab="Year", ylab="Phenotype")
-
-# Add a legend to distinguish between patches
-legend("topright", legend=c("Patch 1", "Patch 2"), col=c(rgb((1-0.9)*0.7,0.59,0.8, alpha =0.6), rgb((2-0.9)*0.7,0.59,0.8, alpha =0.6)), pch=19)
-
-# Add a title to the plot
-title("Individual Phenotypes Over Years for Patch 1 and Patch 2")
-
-# -------------------------------
 
 
 
