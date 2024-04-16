9ab4e215d78f790ddcbfca666b86a986be484b60
Izabel <erikssonizabel@gmail.com>
2024-04-16 13:54:31 +0200

    Plotting two resource sim. And some moving around.

--- TheOneTrueSim.R
+++ TheOneTrueSim.R
@@ -930,7 +930,7 @@ job::job(two.res.sym = {
 
 # Asymmteric
 
-job::job(two.res,asym = {
+job::job(two.res.asym = {
   
   rep <- 3
   
@@ -1845,6 +1845,137 @@ plots + plot_annotation(
 
 
 #-------------------------------------------------
+
+
+# Plotting two resource sim -----------------------
+
+# Symmetric
+
+Res <- list()
+
+pdf("plots.2res.sym.pdf")
+
+for(s in 1:length(sigma)){
+  adu.sigma <- sigma[s]
+  
+  
+  juv.sigma <- adu.sigma
+  
+  
+  last.year.list.even <- data.frame()
+  
+  for(i in 1:length(two.res.sym$Total.endpoint.CLC.even)){
+    this.run <- two.res.sym$Total.endpoint.CLC.even[[i]][[s]]
+    this.run$run <- i
+    this.run$Adult_sigma <- adu.sigma
+    this.run$Juvenile_sigma <- juv.sigma
+    last.year.list.even <- rbind(last.year.list.even, this.run)
+  }
+
+  
+  
+  plot.list.even <- list()
+  
+  for (i in 1:3){
+    
+    data <- last.year.list.even[last.year.list.even$run == i, ]
+    
+    color.palette <- plasma(length(data$Adult_Trait))
+    
+    plot.list.even[[i]] <- ggplot(data, aes(x = Juvenile_Trait, y = Adult_Trait)) +
+      geom_point(aes(size=Num_Individuals), color = color.palette, show.legend = FALSE) + 
+      labs(title = substitute(sigma == value, list(value = adu.sigma)), x = "Juvenile Trait", y = "Adult Trait", size = "Number of individuals") +                 # Labels for the axes
+      scale_x_continuous(limits = c(-3, 3))+
+      scale_y_continuous(limits = c(-3, 3))+
+      scale_size_continuous(limits=c(1,40000),breaks=c(seq(from = 0, to = 40000, by = 5000))) +
+      theme(aspect.ratio=1) +
+      theme_minimal(base_family = "LM Roman 10", base_size = 10)
+    
+    
+  }
+  
+  
+  plots <- wrap_plots(plot.list.even)
+  
+  
+  
+  Res[[s]] <- plots + plot_annotation(
+    title = 'Even Distribution',
+    theme = theme(plot.title = element_text(hjust = 0.5, size = 15, family = "LM Roman 10"), plot.subtitle = element_text(hjust = 0.5, size = 15, family = "LM Roman 10"))
+  )+ coord_fixed()
+  
+  print(Res[[s]])
+  
+  
+} 
+
+dev.off()
+
+
+# Asymmetric
+
+Res <- list()
+
+pdf("plots.2res.asym.pdf")
+
+for(s in 1:length(sigma)){
+  adu.sigma <- sigma[s]
+  
+  
+  juv.sigma <- adu.sigma
+  
+  
+  last.year.list.even <- data.frame()
+  
+  for(i in 1:length(two.res.asym$Total.endpoint.CLC.even)){
+    this.run <- two.res.asym$Total.endpoint.CLC.even[[i]][[s]]
+    this.run$run <- i
+    this.run$Adult_sigma <- adu.sigma
+    this.run$Juvenile_sigma <- juv.sigma
+    last.year.list.even <- rbind(last.year.list.even, this.run)
+  }
+  
+  
+  
+  plot.list.even <- list()
+  
+  for (i in 1:3){
+    
+    data <- last.year.list.even[last.year.list.even$run == i, ]
+    
+    color.palette <- plasma(length(data$Adult_Trait))
+    
+    plot.list.even[[i]] <- ggplot(data, aes(x = Juvenile_Trait, y = Adult_Trait)) +
+      geom_point(aes(size=Num_Individuals), color = color.palette, show.legend = FALSE) + 
+      labs(title = substitute(sigma == value, list(value = adu.sigma)), x = "Juvenile Trait", y = "Adult Trait", size = "Number of individuals") +                 # Labels for the axes
+      scale_x_continuous(limits = c(-3, 3))+
+      scale_y_continuous(limits = c(-3, 3))+
+      scale_size_continuous(limits=c(1,40000),breaks=c(seq(from = 0, to = 40000, by = 5000))) +
+      theme(aspect.ratio=1) +
+      theme_minimal(base_family = "LM Roman 10", base_size = 10)
+    
+    
+  }
+  
+  
+  plots <- wrap_plots(plot.list.even)
+  
+  
+  
+  Res[[s]] <- plots + plot_annotation(
+    title = 'Even Distribution',
+    theme = theme(plot.title = element_text(hjust = 0.5, size = 15, family = "LM Roman 10"), plot.subtitle = element_text(hjust = 0.5, size = 15, family = "LM Roman 10"))
+  )+ coord_fixed()
+  
+  print(Res[[s]])
+  
+  
+} 
+
+dev.off()
+
+# ------------------------------------------------
+
 
 
 # Export data -----------------------------------
