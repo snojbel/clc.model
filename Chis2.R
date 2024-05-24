cb3724b900a5956d7aaddb38e74d70c7228236db
Izabel <erikssonizabel@gmail.com>
2024-05-24 12:30:10 +0200

    Sorting

--- Chis2.R
+++
@@ -1,9 +0,0 @@
-# Statistics test
-
-species <- rbind(two.res.sym$Total.species.CLC.even, two.res.assym$Total.species.CLC.even)
-rownames <- c("Symmetric", "Asymmteric")
-
-chisq.test(species)
-
-
-
